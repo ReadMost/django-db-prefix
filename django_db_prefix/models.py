139b0e833f62940737f7ce772ae082f19196caee
readmost <rauan.amageldiyev@nu.edu.kz>
2021-09-22 12:40:20 +0600

    add functionality to specify prefix per app or per model with consideration abstract=True model inheritance

--- django_db_prefix/models.py
+++ django_db_prefix/models.py
@@ -4,9 +4,18 @@ from django.db.models.signals import class_prepared, pre_init
 
 def add_db_prefix(sender, **kwargs):
     prefix = getattr(settings, "DB_PREFIX", None)
+    if isinstance(prefix, dict):
+        app_label = sender._meta.app_label.lower()
+        sender_name = sender._meta.object_name.lower()
+        full_name = app_label + "." + sender_name
+        if full_name in prefix:
+            prefix = prefix[full_name]
+        elif app_label in prefix:
+            prefix = prefix[app_label]
+        else:
+            prefix = prefix.get(None, None)
     if prefix and not sender._meta.db_table.startswith(prefix):
         sender._meta.db_table = prefix + sender._meta.db_table
-
 
 pre_init.connect(add_db_prefix)
 class_prepared.connect(add_db_prefix)
